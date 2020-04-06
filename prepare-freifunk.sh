21dd4bf069fb81c1b73898dc49e2527121ae5e3e
Panagiotis Vasilopoulos <hello@alwayslivid.com>
2020-04-06 15:55:03 +0300

    Remove references to dn42 from Freifunk config

--- prepare-freifunk.sh
+++ prepare-freifunk.sh
@@ -11,5 +11,5 @@ git clone https://github.com/freifunk/icvpn-meta.git
 # update configuration file
 cd icvpn-scripts
 rm ../etc/dnsmasq.d/freifunk.conf
-./mkdns -f dnsmasq -s ../icvpn-meta/ >> ../etc/dnsmasq.d/freifunk.conf
+./mkdns -f dnsmasq -s ../icvpn-meta/ -x dn42 >> ../etc/dnsmasq.d/freifunk.conf
 cd ..
